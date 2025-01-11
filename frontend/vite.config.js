1f2061c47907d90c733d5f2b95b876ee34614ddc
Kuhu Khandelwal <149239228+kuhu2707@users.noreply.github.com>
2025-01-11 21:53:15 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,32 +1,9 @@
 // import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server:{port:5173}
-// })
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     port: 5173,
-//     proxy:{
-//       '/api': {
-//         target: 'https://ecommerce-webiste-939r.vercel.app',
-//         changeOrigin: true,
-//     }, // Port for development server
-//   },
-//   build: {
-//     sourcemap: false, // Disable source maps in production
-//     minify: 'esbuild', // Use esbuild for minification
-//   },
-// }});
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base:'/',
   plugins: [react()],
     server: {
         proxy: {
