aae277b01eaf1b0c689544f3d4c4e7d9baeaf30a
Kuhu Khandelwal <149239228+kuhu2707@users.noreply.github.com>
2025-01-11 18:52:31 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,23 +10,18 @@ import orderRouter from './routes/orderRoute.js'
 //import {router} from 'router'
 
 const app = express()
+//app.use(cors({origin:"https://ecomf3.onrender.com"}));
+app.use(cors())
+const router = express.Router();
+router.options("/", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin")
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+   });
 
-// const router = express.Router();
-// router.options("/", (req, res) => {
-//   res.setHeader("Access-Control-Allow-Origin", "https://ecomf-ecru.vercel.app/")
-//   res.setHeader("Access-Control-Allow-Credentials", "true");
-//   res.setHeader("Access-Control-Max-Age", "1800");
-//   res.setHeader("Access-Control-Allow-Headers", "content-type");
-//   res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
-//    });
 
-//    app.use(cors({
-//     origin: 'https://ecomf-ecru.vercel.app/', // Replace with your frontend's URL when deployed
-//     credentials: true, // Allow cookies or authorization headers
-// }));
-app.use(cors({origin:"https://ecomf3.onrender.com"}));
-//  app.
-// app config
 
 const port = process.env.PORT || 4000
 connectDB()
