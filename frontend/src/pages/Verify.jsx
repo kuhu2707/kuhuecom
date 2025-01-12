aaa25f98f6427ebcae5d17330d0e5a68fe9c2be0
Kuhu Khandelwal <149239228+kuhu2707@users.noreply.github.com>
2025-01-12 18:28:11 +0530

    Update Verify.jsx

--- frontend/src/pages/Verify.jsx
+++ frontend/src/pages/Verify.jsx
@@ -20,7 +20,7 @@ const Verify = () => {
             return null
         }
 
-        const response = await axios.post('https://ecom-b1.vercel.app/api/order/verifyStripe' , {success,orderId},{headers:{token}})
+        const response = await axios.post('https://kuhuecom.vercel.app/api/order/verifyStripe' , {success,orderId},{headers:{token}})
 
         if(response.data.success){
             setCartItems({})
