d25f42e900e9376bdf0a1e55492c0e6aaeed8142
Kuhu Khandelwal <149239228+kuhu2707@users.noreply.github.com>
2025-01-13 21:44:29 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './App.jsx'
 import './index.css'
 import {BrowserRouter} from 'react-router-dom'
 import ShopContextProvider from './context/ShopContext.jsx'
@@ -8,10 +8,10 @@ import ShopContextProvider from './context/ShopContext.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <BrowserRouter>
      <ShopContextProvider>
         <App />
      </ShopContextProvider>
-  </<React.StrictMode>,
+  </BrowserRouter>,
 )
 
