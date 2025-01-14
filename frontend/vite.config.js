35f2f088737850d192abc8ff345fdbac45d74c04
Kuhu Khandelwal <149239228+kuhu2707@users.noreply.github.com>
2025-01-14 23:52:49 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,33 @@
-// import { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite';
-
 export default defineConfig({
- base:'/',
-plugins: [React()],
-    server: {
-        proxy: {
-            '/api': {
-                target:'https://kuhuecom.vercel.app/',
-                changeOrigin: true,
-                secure: false,
-            },
-        },
-    },
-});
+  plugins: [react()],
+  build:{
+    outDir:'dist',
+  }
+})
+
+
+
+
+
+// // import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite';
+
+// export default defineConfig({
+//  base:'/',
+// plugins: [React()],
+//     server: {
+//         proxy: {
+//             '/api': {
+//                 target:'https://kuhuecom.vercel.app/',
+//                 changeOrigin: true,
+//                 secure: false,
+//             },
+//         },
+//     },
+// });
 
 
 
