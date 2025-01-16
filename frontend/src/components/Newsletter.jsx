c0ee1e97ab9596bbb297f8186133fdbfd8e79f0c
Kuhu Khandelwal <149239228+kuhu2707@users.noreply.github.com>
2025-01-17 00:42:55 +0530

    Update Newsletter.jsx

--- frontend/src/components/Newsletter.jsx
+++ frontend/src/components/Newsletter.jsx
@@ -10,7 +10,7 @@ const Newsletter = () => {
     <div className='text-center'>
       <p className='text-2xl font-medium text-gray-800'>Subcribe now and get 20% off</p>
       <p className='text-gray-400 mt-3'>
-      Lorem Ipsum, is simply dummy text of the printing and typesetting industry.
+      
       </p>
       <form onSubmit={onSubmitHandler} className='w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3'>
         <input className='w-full sm:flex-1 outline-none' type="email"  placeholder='Enter your email' required/>
