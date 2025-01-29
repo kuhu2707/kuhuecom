1797b8788c55dd9c830593e1211d0b705fdca83e
Kuhu Khandelwal <149239228+kuhu2707@users.noreply.github.com>
2025-01-29 18:39:26 +0530

    Update assets.js

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -73,10 +73,10 @@ import contact_img from './contact_img.png'
 import razorpay_logo from './razorpay_logo.png'
 import stripe_logo from './stripe_logo.png'
 import cross_icon from './cross_icon.png'
-import logo2 from './logo2.png'
+import logo21 from './logo21.png'
 
 export const assets = {
-    logo2,
+    logo21,
     logo,
     hero_img,
     cart_icon,
