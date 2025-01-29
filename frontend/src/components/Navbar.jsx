558a8d934f99af6834440bcc5725f82572079c22
Kuhu Khandelwal <149239228+kuhu2707@users.noreply.github.com>
2025-01-29 17:19:02 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
 
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
-      <Link to='/'><img src={assets.logo} className='w-36' alt=""/></Link>
+      <Link to='/'><img src={assets.logo2} className='w-48 h-30 object-contain' alt=""/></Link>
 
       <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
           
